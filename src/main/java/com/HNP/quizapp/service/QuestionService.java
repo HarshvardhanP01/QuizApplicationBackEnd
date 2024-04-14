bafc90803c7942292c4b329dcf7e32ee6dc75f1d
Harshvardhan Nishikant Patil <74046125+HarshvardhanP01@users.noreply.github.com>
2024-04-15 00:39:15 +0530

    Add files via upload

---
+++ src/main/java/com/HNP/quizapp/service/QuestionService.java
@@ -0,0 +1,46 @@
+package com.shubham.quizapp.service;
+
+import com.shubham.quizapp.model.Question;
+import com.shubham.quizapp.dao.QuestionDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+// We can also have @Component but @Services also so the same
+@Service
+public class QuestionService {
+
+    @Autowired
+    QuestionDao questionDao;
+    public ResponseEntity<List<Question>> getAllQuestions() {
+        try{
+            return new ResponseEntity<>(questionDao.findAll(), HttpStatus.OK);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return new ResponseEntity<>(new ArrayList<>(),HttpStatus.BAD_REQUEST);
+    }
+
+    public ResponseEntity<List<Question>> getQuestionsByCategory(String category) {
+        try{
+            return new ResponseEntity<>(questionDao.findByCategory(category), HttpStatus.OK);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return new ResponseEntity<>(new ArrayList<>(),HttpStatus.BAD_REQUEST);
+    }
+
+    public ResponseEntity<String> addQuestion(Question question) {
+        try{
+            questionDao.save(question);
+            return new ResponseEntity<>("Data added Successfully!!", HttpStatus.CREATED);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return new ResponseEntity<>("Failed to add Data!!",HttpStatus.BAD_REQUEST);
+    }
+}
